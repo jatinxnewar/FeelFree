c1cea0d5d8e44a82c98d8635d8a48e1159300210
JATIN NEWAR <132223040+jatinxnewar@users.noreply.github.com>
2025-08-02 23:54:16 +0530

    fixes

--- src/components/common/FeatureCard.jsx
+++ src/components/common/FeatureCard.jsx
@@ -2,10 +2,10 @@ import { Link } from 'react-router-dom'
 import './FeatureCard.css'
 
 function FeatureCard({ feature }) {
-  const { icon, title, description, link, color } = feature
+  const { icon, title, description, link } = feature
 
   return (
-    <div className="feature-card" style={{ '--accent-color': color }}>
+    <div className="feature-card">
       <div className="feature-icon">
         {icon}
       </div>
@@ -13,7 +13,10 @@ function FeatureCard({ feature }) {
         <h3 className="feature-title">{title}</h3>
         <p className="feature-description">{description}</p>
         <Link to={link} className="feature-link">
-          Explore <span className="arrow">→</span>
+          Explore 
+          <svg viewBox="0 0 20 20" fill="currentColor">
+            <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd" />
+          </svg>
         </Link>
       </div>
     </div>
